a78bd646be8d4e054e58f9e9a10e3eacf510f81b
arehman <arehman@acrobits.cz>
2024-04-03 09:23:49 +0500

    spelling mistake

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
                       url: "https://dist.acrobits.net/webp-package/SharpYuv.xcframework.zip",
                       checksum: "3691e1345018f488221f7bfdd682d27d2602fcaf47fec53e355de7099e3fa1fd"),
         .binaryTarget(name: "Webp",
-                      url: "https://dist.acrobits.net/webp-package/WebP.xcframework.zip",
+                      url: "https://dist.acrobits.net/webp-package/Webp.xcframework.zip",
                       checksum: "ee9dc2a26fdbcaa100197a152abe04f058e51e13dfea21713795109cda4995f9")
     ]
 )
